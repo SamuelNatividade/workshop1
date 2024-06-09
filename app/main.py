d2a0db26ace937f3c2bdcdbbb3634ffc5dcb0ae2
samuel_natividade <samuell.natividade@gmail.com>
2024-06-09 19:38:06 -0300

    adicionando load

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 from pipeline.extract import extract_from_excel 
+from pipeline.transform import concat_data_frames
+from pipeline.load import load_excel
 
-listas_de_dataframes = extract_from_excel(path = "/Users/samuelnatividade/Desktop/Projetos/workshop1/data/input")
-print(listas_de_dataframes)
+if __name__ == "__main__":
+    # extract
+    listas_de_dataframes = extract_from_excel(path = "/Users/samuelnatividade/Desktop/Projetos/workshop1/data/input")
+    # transform 
+    df_concat = concat_data_frames(listas_de_dataframes)
+    # load
+    load_excel(data_frame = df_concat, output_path = "/Users/samuelnatividade/Desktop/Projetos/workshop1/data/output", file_name = 'output_data')
---
+++ app/pipeline/load.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> str:
+    """
+    receber um dataframe e salvar como excel
+    return "Arquivo salvo com sucesso"
+    """
+    data_frame.to_excel(f"{output_path}/{file_name}.xlsx", index = False)
+    return "Arquivo salvo com sucesso"
